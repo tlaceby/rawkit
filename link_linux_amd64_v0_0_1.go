0e00fc10fcd8a8cae9fbdf8e245792bd023f39c0
Tyler Laceby <tylerbadgertutorials@gmail.com>
2025-06-24 19:03:34 -0600

    release v0.0.2

---
+++ link_linux_amd64_v0_0_1.go
@@ -0,0 +1,10 @@
+//go:build linux && amd64
+
+package rawkit
+
+/*
+#cgo LDFLAGS: -L${SRCDIR}/libs/linux_amd64/v0.0.1 -lraw_wrapper -lraw -lz -lm -lstdc++
+#include <stdlib.h>
+#include "wrapper/libraw_wrapper.h"
+*/
+import "C"
---
+++ link_windows_amd64_v0_0_1.go
@@ -0,0 +1,10 @@
+//go:build windows && amd64
+
+package rawkit
+
+/*
+#cgo LDFLAGS: -L${SRCDIR}/libs/windows_amd64/v0.0.1 -lraw_wrapper -lraw -lz -lm -lstdc++
+#include <stdlib.h>
+#include "wrapper/libraw_wrapper.h"
+*/
+import "C"
