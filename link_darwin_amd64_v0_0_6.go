5a3271472a9df63b62b86ef06eba9712b618f90d
Tyler Laceby <tylerbadgertutorials@gmail.com>
2025-06-24 18:45:23 -0600

    release

---
+++ link_darwin_amd64_v0_0_6.go
@@ -0,0 +1,10 @@
+//go:build darwin && amd64
+
+package rawkit
+
+/*
+#cgo LDFLAGS: -L${SRCDIR}/libs/darwin_amd64/v0.0.6 -lraw_wrapper -lraw -lz -lm -lc++
+#include <stdlib.h>
+#include "wrapper/libraw_wrapper.h"
+*/
+import "C"
