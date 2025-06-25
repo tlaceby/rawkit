38ad90ced0e9b98e2336d97cb1d4dda4e4903e74
Tyler Laceby <tylerbadgertutorials@gmail.com>
2025-06-24 19:12:50 -0600

    release v0.0.5

--- link_linux_amd64_v0_0_5.go
+++ link_linux_amd64_v0_0_5.go
@@ -3,7 +3,7 @@
 package rawkit
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/libs/linux_amd64/v0.0.4 -lraw_wrapper -lraw -lz -lm -lstdc++
+#cgo LDFLAGS: -L${SRCDIR}/libs/linux_amd64/v0.0.5 -lraw_wrapper -lraw -lz -lm -lstdc++
 #include <stdlib.h>
 #include "wrapper/libraw_wrapper.h"
 */
